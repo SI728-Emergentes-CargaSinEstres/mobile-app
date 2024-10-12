765b7d1fd9537c4b622f11b2bff53f967f60f83a
Lucero <u202111465@upc.edu.pe>
2024-10-11 23:07:11 -0500

    feat: Add home screen

---
+++ carga_sin_estres_flutter/lib/screens/menu/home.dart
@@ -0,0 +1,69 @@
+import 'package:carga_sin_estres_flutter/utils/theme.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: AppTheme.primaryWhite,
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Image.asset(
+                  'assets/background/wave_up_3.png',
+                  width: screenWidth,
+                  fit: BoxFit.cover,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      _buildUserWave('Javier'),
+                      const SizedBox(height: 55),
+                    ],
+                  ),
+                ),
+              ]),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserWave(String username) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: RichText(
+        textAlign: TextAlign.left,
+        text: TextSpan(
+          style: const TextStyle(
+            fontSize: 24,
+            color: AppTheme.secondaryBlack,
+          ),
+          children: <TextSpan>[
+            const TextSpan(text: 'Bienvenido '),
+            TextSpan(
+              text: '$username 👋',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
